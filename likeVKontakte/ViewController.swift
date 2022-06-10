ff407dafd57be641624b31fa1865523f0471b3f2
GolubeykovAnton <ds6580@yandex.ru>
2022-06-10 15:05:03 +0300

    Initial Commit

---
+++ likeVKontakte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  likeVKontakte
+//
+//  Created by Антон Голубейков on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
