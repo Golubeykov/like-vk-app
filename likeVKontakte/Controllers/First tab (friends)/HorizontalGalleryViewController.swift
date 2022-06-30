a5d57bb86953f18c4b0fdcf3a6dc220a28be96c6
GolubeykovAnton <ds6580@yandex.ru>
2022-06-30 12:48:40 +0300

    Первая верстка экрана горизонтальной галереи

--- likeVKontakte/Controllers/First tab (friends)/HorizontalGalleryViewController.swift
+++ likeVKontakte/Controllers/First tab (friends)/HorizontalGalleryViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class HorizontalGalleryViewController: UIViewController {
 
+    @IBOutlet weak var horizontalGalleryView: HorizontalGalleryView!
     var filteredPhotos: [String] = []
     
     override func viewDidLoad() {
+        var photos: [UIImage] = []
+        for photoName in filteredPhotos {
+            if let photo = UIImage(named: photoName) {
+                photos.append(photo)
+            }
+        }
         super.viewDidLoad()
         print(filteredPhotos)
     }
--- likeVKontakte/Views/HorizontalGalleryView.swift
+++ likeVKontakte/Views/HorizontalGalleryView.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 @IBDesignable class HorizontalGalleryView: UIView {
-    var horizontalGalleryView: UIView!
+    @IBInspectable var inactiveIndicatorColor: UIColor = UIColor.lightGray
+    @IBInspectable var activeIndicatorColor: UIColor = UIColor.black
+    
+    private var horizontalGalleryView: UIView!
+    
+    private var interactiveAnimator: UIViewPropertyAnimator!
+    
+    private var mainImageView = UIImageView()
+    private var secondaryImageView = UIImageView()
+    private var images = [UIImage]()
+    private var isLeftSwipe = false
+    private var isRightSwipe = false
+    private var chooseFlag = false
+    private var currentIndex = 0
+    private var customPageView = UIPageControl()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -33,5 +47,35 @@ import UIKit
         self.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         self.addSubview(self.horizontalGalleryView)
         self.bringSubviewToFront(horizontalGalleryView)
+        
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        self.addGestureRecognizer(recognizer)
+        
+        mainImageView.backgroundColor = UIColor.systemYellow
+        mainImageView.frame = self.bounds
+        addSubview(mainImageView)
+        
+        secondaryImageView.backgroundColor = UIColor.systemGreen
+        secondaryImageView.frame = self.bounds
+        secondaryImageView.transform = CGAffineTransform(translationX: UIScreen.main.bounds.width, y: 0)
+        addSubview(secondaryImageView)
+        
+        //листалка внизу вьюхи
+        customPageView.backgroundColor = UIColor.clear
+        customPageView.frame = CGRect(x: 1, y: 1, width: 150, height: 50)
+        customPageView.layer.zPosition = 100
+        customPageView.numberOfPages = 1
+        customPageView.currentPage = 0
+        customPageView.pageIndicatorTintColor = self.inactiveIndicatorColor
+        customPageView.currentPageIndicatorTintColor = self.activeIndicatorColor
+        addSubview(customPageView)
+        
+        customPageView.translatesAutoresizingMaskIntoConstraints = false
+        customPageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        customPageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -self.bounds.height / 15).isActive = true
+        
+    }
+    @objc func onPan(_ recognizer: UIPanGestureRecognizer) {
+        
     }
 }
